494c98622bcfa3b911c299fdbc2e35e6a2f5e1fa
havesomepham <23087717+havesomepham@users.noreply.github.com>
2023-02-04 02:16:20 -0500

    implemented mean calculation version C small-area

--- test.py
+++ test.py
@@ -12,21 +12,33 @@ while (True):
     # Capture the video frame
     # by frame
     ret, frame = vid.read()
-    
+
     # mean calculation version A (total mean)
     # r = frame[:, :, :1]
     # g = frame[:, :, 1:2]
     # b = frame[:, :, 2:]
-    
+
     # r_mean = np.mean(r)
     # g_mean = np.mean(g)
     # b_mean = np.mean(b)
-    
+
     # mean calculation version B (single-pixel)
-    r_mean = frame[int(height/2)][int(width/2)][0]
-    g_mean = frame[int(height/2)][int(width/2)][1]
-    b_mean = frame[int(height/2)][int(width/2)][2]
-    
+    # r_mean = frame[int(height/2)][int(width/2)][0]
+    # g_mean = frame[int(height/2)][int(width/2)][1]
+    # b_mean = frame[int(height/2)][int(width/2)][2]
+
+    # mean calculation version C (small-area)
+    ymin = int(height/2 - 20)
+    ymax = int(height/2 + 20)
+    xmin = int(width/2 - 20)
+    xmax = int(width/2 + 20)
+    r = frame[ymin:ymax, xmin:xmax, 0]
+    g = frame[ymin:ymax, xmin:xmax, 1]
+    b = frame[ymin:ymax, xmin:xmax, 2]
+
+    r_mean = np.mean(r)
+    g_mean = np.mean(g)
+    b_mean = np.mean(b)
     pixel = np.array([r_mean, g_mean, b_mean])
 
     frame[:] = pixel
